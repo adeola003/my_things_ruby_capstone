edb0abe2162a1ecec2b341025eb79f8bd0f6937a
adeola003 <khadaf3@gmail.com>
2023-07-03 08:00:17 +0100

    install linters and add readme

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'rspec', '~> 3.12'
+
+gem 'rubocop', '>= 1.0', '< 2.0'
--- main.rb
+++ main.rb
@@ -1,25 +1,25 @@
 require_relative 'item'
 
 def print_menu
-  puts "Options:"
-  puts "1. List all items"
-  puts "2. Move item to archive"
-  puts "3. Quit"
+  puts 'Options:'
+  puts '1. List all items'
+  puts '2. Move item to archive'
+  puts '3. Quit'
 end
 
 def list_all_items(items)
-  puts "Items:"
+  puts 'Items:'
   items.each do |item|
     puts "ID: #{item.id}, Title: #{item.title}, Published Date: #{item.published_date}, Archived: #{item.archived}"
   end
 end
 
 def move_item_to_archive(items)
-  puts "Enter the ID of the item to move to the archive:"
+  puts 'Enter the ID of the item to move to the archive:'
   id = gets.chomp.to_i
-  item = items.find { |item| item.id == id }
-  if item
-    item.move_to_archive
+  item_found = items.find { |item| item.id == id }
+  if item_found
+    item_found.move_to_archive
   else
     puts "Item with ID #{id} not found."
   end
@@ -38,6 +38,6 @@ loop do
   when 3
     break
   else
-    puts "Invalid option. Please choose again."
+    puts 'Invalid option. Please choose again.'
   end
 end
