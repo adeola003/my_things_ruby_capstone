cfdf6a8ef0262fc6e34c47554cd255f9af8477b6
adeola003 <khadaf3@gmail.com>
2023-07-03 12:27:51 +0100

    update item class

--- item.rb
+++ item.rb
@@ -1,11 +1,25 @@
 class Item
-  attr_accessor :id, :title, :published_date, :archived
+  attr_accessor :id, :published_date, :archived, :genre, :author, :label
 
-  def initialize(id, title, published_date)
-    @id = id
-    @title = title
+  def initialize(published_date)
+    @id = Random.rand(1..1000)
     @published_date = published_date
     @archived = false
+  end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
   end
 
   def can_be_archived?
