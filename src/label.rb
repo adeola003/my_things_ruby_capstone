ba1dd8d67f805db541a31d43aacabfcb6f1cef7b
adeola003 <khadaf3@gmail.com>
2023-07-03 13:40:12 +0100

    add add_item

--- src/label.rb
+++ src/label.rb
@@ -2,11 +2,13 @@ require_relative 'item'
 class Label < Item
   attr_accessor :title, :color, :items
   #
-  def initialize(title, color, items)
+  def initialize(title, color)
     @title = title
     @color = color
-    @items = items
+    @items = []
   end
 
-  def add_item
+  def add_item(item)
+    items << item
+  end
 end
