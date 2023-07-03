267ec8662fee6751ccfb1e05c48b80832c7fd840
adeola003 <khadaf3@gmail.com>
2023-07-03 13:20:02 +0100

    add label and book classes

--- src/book.rb
+++ src/book.rb
@@ -1,5 +1,11 @@
 require_relative 'item'
 class Book < Item
   #
+  attr_accessor :publisher, :cover_state
+  #
+  def initialize(publisher, cover_state)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
   
 end
--- src/label.rb
+++ src/label.rb
@@ -1,4 +1,12 @@
 require_relative 'item'
 class Label < Item
+  attr_accessor :title, :color, :items
   #
+  def initialize(title, color, items)
+    @title = title
+    @color = color
+    @items = items
+  end
+
+  def add_item
 end
