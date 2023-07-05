5241d9be9c1a639e8a3485ea3aeb97442da086c7
adeola003 <khadaf3@gmail.com>
2023-07-05 13:28:00 +0100

    add schema for books and labels

--- SQL/books_labels.sql
+++ SQL/books_labels.sql
@@ -1,1 +1,18 @@
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  published_date DATE,
+  archived BOOLEAN,
+  genre_id INTEGER,
+  author_id INTEGER,
+  label_id INTEGER,
+  FOREIGN KEY (genre_id) REFERENCES genres(id),
+  FOREIGN KEY (author_id) REFERENCES authors(id),
+  FOREIGN KEY (label_id) REFERENCES labels(id)
+);
+
+CREATE TABLE labels (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  color TEXT
+);
 
